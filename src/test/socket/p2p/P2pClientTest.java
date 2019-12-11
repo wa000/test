c80fb6274b5ad13b9ca13e87aff4830584f18c3e
wa000 <w603257390@163.com>
2019-12-11 17:30:41 +0800

    p2p测试

--- src/test/socket/p2p/P2pClientTest.java
+++ src/test/socket/p2p/P2pClientTest.java
@@ -1,102 +1,79 @@
 package test.socket.p2p;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class P2pClientTest
 {
     private static int SERVER_PORT = 20880;
     //    private static String SERVER_IP = "47.104.28.163";
     private static String SERVER_IP = "127.0.0.1";
-    public static Map<String, String> map = new HashMap<String, String>();
 
     public static void main(String args[]) throws Exception {
+
+        System.out.println("输入客户端名：");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String clientName = reader.readLine();
+
+        int port = 20881;
+        if("2".equals(clientName))
+        {
+            port = 20882;
+        }
+
         SocketAddress server = new InetSocketAddress(SERVER_IP, SERVER_PORT);
         DatagramSocket ds = new DatagramSocket();
-        String str = "getIp";
-        byte buff[] = str.getBytes();
-        byte buffer[] = new byte[8 * 1024];
-        DatagramPacket dp = new DatagramPacket(buff, 0, buff.length, server);
+        String str = "getIp" + "@_@" + clientName;
 
-        Scanner sc = new Scanner(System.in);
+        String message = sendMessage(str, server, port);
+        System.out.println("本机端口、IP: " + message);
 
-        ds.send(dp);//发送信息到服务器
-        dp.setData(buffer, 0, 8 * 1024);
-        ds.receive(dp);
-        String message = new String(dp.getData(), 0, dp.getLength());
-        System.out.println("本机端口、IP" + message);
+        System.out.println("输入需要连接的客户端名：");
+        clientName = reader.readLine();
 
-        MyThread1 mt = new MyThread1(ds);
-        Thread thread = new Thread(mt);
-        thread.start();
-
-        while (true) {
-            str = sc.next();
-            if (str.startsWith("aa")) {
-                buff = str.getBytes();
-                dp.setData(buff, 0, buff.length);
-                ds.send(dp);
-            }
-            else
-            {
-                String tempString = str;
-                Set<String> set = map.keySet();
-                Iterator<String> iter = set.iterator();
-                while (iter.hasNext()) {
-                    String key = iter.next();
-                    String value = map.get(key);
-                    tempString = "sendMessage" + "," + key + "," + value + "," + tempString;
-                    buff = tempString.getBytes();
-                    dp.setData(buff, 0, buff.length);
-                    ds.send(dp);
-                    System.out.println("发送成功");
-                }
-            }
+        String socketInfo = "";
+        while("".equals(socketInfo))
+        {
+            System.out.println("等待连接客户端:" + clientName);
+            Thread.sleep(1000);
+            socketInfo = sendMessage("getSocket@_@" + clientName, server, port);
         }
-    }
-}
+        System.out.println(socketInfo);
 
-class MyThread1 implements Runnable {
-    DatagramSocket ds = null;
-    DatagramPacket dp = null;
-    String message = "";
+        System.out.println("开始socket连接");
 
-    public MyThread1(DatagramSocket ds) {
-        this.ds = ds;
     }
 
-    public void run() {
-        try {
-            byte buffer[] = new byte[1024];
-            dp = new DatagramPacket(buffer, 0, buffer.length);
-            byte buff[] = new byte[1024];
-            while (true) {
-                dp.setData(buff, 0, 1024);
+    /**
+     * 发送UDP请求
+     *
+     * @param str
+     * @param server
+     * @return
+     */
+    private static String sendMessage(String str, SocketAddress server, int port)
+    {
+        try
+        {
+            DatagramSocket ds = new DatagramSocket(port);
+            byte buff[] = str.getBytes();
+            byte buffer[] = new byte[8 * 1024];
+            DatagramPacket dp = new DatagramPacket(buff, 0, buff.length, server);
 
-                ds.receive(dp);
-                message = new String(dp.getData(), 0, dp.getLength());
-                if (message.startsWith("udp")) {
-                    System.out.println("准备接收");
-                    String port = message.split(",")[1];
-                    String ip = message.split(",")[2];
-                    P2pClientTest.map.put(port, ip);
-                    System.out.println("目标端口、IP:" + port + "," + ip);
-                }
-                else if(message.startsWith("603257390"))
-                {
+            ds.send(dp);//发送信息到服务器
+            dp.setData(buffer, 0, 8 * 1024);
+            ds.receive(dp);
+            String message = new String(dp.getData(), 0, dp.getLength());
 
-                }
-                else
-                {
-                    System.out.println("准备接收");
-                    System.out.println("接收到" + message);
-                }
-            }
-        } catch (Exception e) {
+            ds.close();
+            return message;
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
+            return null;
         }
     }
+
 }
--- src/test/socket/p2p/P2pServerTest.java
+++ src/test/socket/p2p/P2pServerTest.java
@@ -1,139 +1,78 @@
 package test.socket.p2p;
 
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * P2P服务端，负责接收连接消息
+ */
 public class P2pServerTest
 {
-    public static Map<String,String> map=new HashMap<String,String>();
+    private static Map<String, String> socketCache = new HashMap<>();
+
     public static void main(String args[]) throws Exception
     {
 
-        int clientPort=0;
-        String ip="";
+        int clientPort = 0;
+        String ip = "";
+
+        // 开启20880端口的udp监听服务
         final DatagramSocket ds = new DatagramSocket(20880);
-        String message="";
-        String str="";
-        byte by[]=new byte[2000];
-        DatagramPacket dp=new DatagramPacket(by,0,by.length);
+        String message = "";
+        String str = "";
+        byte by[] = new byte[2000];
+        DatagramPacket dp = new DatagramPacket(by, 0, by.length);
         SocketAddress socketAddress = null;
         InetSocketAddress inetSocketAddress = null;
+
+        // 开始监听
         while(true)
         {
             System.out.println("wait...");
             ds.receive(dp);
             System.out.println("success");
-            message=new String(dp.getData(),0,dp.getLength());
-            System.out.println("message:"+message);
-            socketAddress=dp.getSocketAddress();
+            message = new String(dp.getData(),0, dp.getLength());
+            System.out.println("message:" + message);
+            socketAddress = dp.getSocketAddress();
             inetSocketAddress = (InetSocketAddress) socketAddress;
-            clientPort=inetSocketAddress.getPort();
+            clientPort = inetSocketAddress.getPort();
             ip=inetSocketAddress.getAddress().getHostAddress();
             if(message.startsWith("getIp"))
             {
-                map.put(String.valueOf(clientPort),ip);
-                str=clientPort+","+ip;
-                byte buf[]=str.getBytes();
-                dp.setData(buf,0,buf.length);
-                ds.send(dp);
-                System.out.println(clientPort+","+ip);
-            }
-            if(message.startsWith("aa"))
-            {
-                Set<String> set=map.keySet();
-                Iterator<String> iter=set.iterator();
-                while(iter.hasNext())
+                str = clientPort + "," + ip;
+
+                if(message.contains("@_@"))
                 {
-                    String key=iter.next();
-                    String value=map.get(key);
-                    System.out.println(key+"clientPort"+clientPort);
-                    str="udp"+","+key+","+value;
-                    byte buf[]=str.getBytes();
-                    dp.setData(buf,0,buf.length);
-                    ds.send(dp);
-                    System.out.println("send");
-//                    if(clientPort==Integer.valueOf(key))
-//                    {
-//                    }
+                    String socketName = message.split("@_@")[1];
+
+                    socketCache.put(socketName, str);
                 }
-            }
-            if(message.startsWith("sendMessage"))
-            {
-                final String port = message.split(",")[1];
-                final String targetIp = message.split(",")[2];
-                str=message.split(",")[3];
+
                 byte buf[] = str.getBytes();
-                SocketAddress sa = new InetSocketAddress(targetIp, Integer.valueOf(port));
-                DatagramPacket target = new DatagramPacket(buf,0, buf.length, sa);
-                ds.send(target);
+                dp.setData(buf,0, buf.length);
+                ds.send(dp);
+                System.out.println(clientPort + "," + ip);
+            }
 
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        for(int i = 0; i < 1000; i++)
-                        {
-                            byte buf[] = ("wa00" + i + ": " + new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date())).getBytes();
-                            SocketAddress sa = new InetSocketAddress(targetIp, Integer.valueOf(port));
-                            DatagramPacket target = new DatagramPacket(buf,0, buf.length, sa);
-                            try
-                            {
-                                ds.send(target);
-                            }
-                            catch (IOException e)
-                            {
-                                e.printStackTrace();
-                            }
-
-                            if(i == 0)
-                            {
-                                try {
-                                    Thread.sleep(10000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                            else
-                            {
-                                try {
-                                    Thread.sleep(5 * 60 * 1000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
+            if(message.startsWith("getSocket"))
+            {
+                if(message.contains("@_@"))
+                {
+                    String socketName = message.split("@_@")[1];
+                    String result = socketCache.get(socketName);
+                    if(null == result)
+                    {
+                        result = "";
                     }
-                }).start();
-
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        for(int i = 0; i < 10000000; i++)
-                        {
-                            byte buf[] = "603257390".getBytes();
-                            SocketAddress sa = new InetSocketAddress(targetIp, Integer.valueOf(port));
-                            DatagramPacket target = new DatagramPacket(buf,0, buf.length, sa);
-                            try
-                            {
-                                ds.send(target);
-                            }
-                            catch (IOException e)
-                            {
-                                e.printStackTrace();
-                            }
-
-                            try {
-                                Thread.sleep(1000);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }).start();
+                    byte buf[] = result.getBytes();
+                    dp.setData(buf,0, buf.length);
+                    ds.send(dp);
+                    System.out.println("getSocket返回：" + result);
+                }
             }
 
             dp.setData(by,0,by.length);
