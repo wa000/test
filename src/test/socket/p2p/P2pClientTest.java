7c4e41bc9a464112ce8752584781434ed90c7e21
wa000 <w603257390@163.com>
2019-12-12 17:37:14 +0800

    p2p测试

--- src/test/socket/p2p/P2pClientTest.java
+++ src/test/socket/p2p/P2pClientTest.java
@@ -1,14 +1,15 @@
 package test.socket.p2p;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
-public class P2pClientTest
-{
+public class P2pClientTest {
     private static int SERVER_PORT = 20880;
-    //    private static String SERVER_IP = "47.104.28.163";
-    private static String SERVER_IP = "127.0.0.1";
+    private static String SERVER_IP = "47.104.28.163";
+//    private static String SERVER_IP = "127.0.0.1";
 
     public static void main(String args[]) throws Exception {
 
@@ -17,8 +18,7 @@ public class P2pClientTest
         String clientName = reader.readLine();
 
         int port = 20881;
-        if("2".equals(clientName))
-        {
+        if ("2".equals(clientName)) {
             port = 20882;
         }
 
@@ -33,16 +33,125 @@ public class P2pClientTest
         clientName = reader.readLine();
 
         String socketInfo = "";
-        while("".equals(socketInfo))
-        {
+        while ("".equals(socketInfo)) {
             System.out.println("等待连接客户端:" + clientName);
             Thread.sleep(1000);
             socketInfo = sendMessage("getSocket@_@" + clientName, server, port);
         }
         System.out.println(socketInfo);
+        System.out.println("开始udp打洞");
+        String ipConn = socketInfo.split(",")[1];
+        String portConn = socketInfo.split(",")[0];
+        server = new InetSocketAddress(ipConn, Integer.parseInt(portConn));
+        sendMessage("testUDPdadong", server, port);
 
         System.out.println("开始socket连接");
+        if (port == 20881) {
+            System.out.println(getdate() + "  等待客户端连接...");
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket socket = serverSocket.accept();
+            new sendMessThread1(socket).start();
 
+            System.out.println(getdate() + "  客户端 （" + socket.getInetAddress().getHostAddress() + "） 连接成功...");
+            InputStream in = socket.getInputStream();
+            int len = 0;
+            byte[] buf = new byte[1024];
+            while ((len = in.read(buf)) != -1) {
+                System.out.println(getdate() + "  客户端: （"
+                        + socket.getInetAddress().getHostAddress() + "）说："
+                        + new String(buf, 0, len, "UTF-8"));
+            }
+        } else {
+            System.out.println(getdate() + "  连接服务端...");
+            Socket socket = new Socket();
+            socket.bind(new InetSocketAddress(port));
+            socket.connect(new InetSocketAddress(ipConn, Integer.parseInt(portConn)));
+
+            new sendMessThread2(socket).start();
+
+            InputStream s = socket.getInputStream();
+            byte[] buf = new byte[1024];
+            int len = 0;
+            while ((len = s.read(buf)) != -1) {
+                System.out.println(getdate() + "  服务器说：  " + new String(buf, 0, len, "UTF-8"));
+            }
+        }
+    }
+
+    public static String getdate() {
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String result = format.format(date);
+        return result;
+    }
+
+    static class sendMessThread1 extends Thread {
+        Socket socket = null;
+
+        public sendMessThread1(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            Scanner scanner = null;
+            OutputStream out = null;
+            try {
+                if (socket != null) {
+                    scanner = new Scanner(System.in);
+                    out = socket.getOutputStream();
+                    String in = "";
+                    do {
+                        in = scanner.next();
+                        out.write(("" + in).getBytes("UTF-8"));
+                        out.flush();// 清空缓存区的内容
+                    } while (!in.equals("q"));
+                    scanner.close();
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class sendMessThread2 extends Thread {
+        Socket socket = null;
+
+        public sendMessThread2(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            //写操作
+            Scanner scanner = null;
+            OutputStream os = null;
+            try {
+                scanner = new Scanner(System.in);
+                os = socket.getOutputStream();
+                String in = "";
+                do {
+                    in = scanner.next();
+                    os.write(("" + in).getBytes());
+                    os.flush();
+                } while (!in.equals("bye"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            scanner.close();
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -52,10 +161,9 @@ public class P2pClientTest
      * @param server
      * @return
      */
-    private static String sendMessage(String str, SocketAddress server, int port)
-    {
-        try
-        {
+    private static String sendMessage(String str, SocketAddress server, int port) {
+        try {
+            String message = "";
             DatagramSocket ds = new DatagramSocket(port);
             byte buff[] = str.getBytes();
             byte buffer[] = new byte[8 * 1024];
@@ -63,14 +171,14 @@ public class P2pClientTest
 
             ds.send(dp);//发送信息到服务器
             dp.setData(buffer, 0, 8 * 1024);
-            ds.receive(dp);
-            String message = new String(dp.getData(), 0, dp.getLength());
+            if (!"testUDPdadong".equals(str)) {
+                ds.receive(dp);
+                message = new String(dp.getData(), 0, dp.getLength());
+            }
 
             ds.close();
             return message;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
